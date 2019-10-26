73fc034336a4a441578bbf236492741746e8c41f
Areeg Emarah <ae407@cam.ac.uk>
2019-10-27 00:45:43 +0100

    getting data from billboard working

--- api_billboard.py
+++ api_billboard.py
@@ -1,9 +1,14 @@
 import sys
 import billboard
 import datetime
+import pandas as pd
+import csv
 
-chart = billboard.ChartData('hot-100')
-print(type(chart.previousDate))
+
+start_date = '2015-04-25'
+chart = billboard.ChartData('hot-100', start_date)
+#print(chart)
+#print(type(chart.previousDate))
 #print(datetime.strptime(chart.previousDate))
 
 #print(type(song.title)) - str
@@ -14,27 +19,45 @@ total_chart = []
 total_chart.append(chart)
 print(total_chart)
 
-for i in range(3):
-    chart_1 = billboard.ChartData('hot-100', chart.previousDate)
-    total_chart.append(chart_1)
+for i in range(5):
+    chart = billboard.ChartData('hot-100', chart.previousDate)
+    total_chart.append(chart)
 
 print(total_chart)
 
 
 
-
 def dict_from_chart(chart,dictionary):
 	for i in range (len(chart)):
-		song = chart[i]
-		#print(song.title)
-		#print(dictionary.keys())
-		if not song.title in dictionary.keys():
-			dictionary[song.title] = ''
+			song = chart[i]
+			if not song.title in dictionary.keys():
+				dictionary[song.title] = ''
+
+	return dictionary
+
+
+def dict_from_total_chart(total_chart,dictionary):
+	for chart in total_chart:
+		for i in range (len(chart)):
+			song = chart[i]
+			#print(song.title)
+			#print(dictionary.keys())
+			if not song.title in dictionary.keys():
+				dictionary[song.title] = ''
 
 	return dictionary
 
 dictionary = {}
-print(dict_from_chart(chart,dictionary))
+#print(dict_from_chart(chart,dictionary))
+dict_from_total_chart(total_chart,dictionary)
+print(len(dictionary))
+
+#df = pd.DataFrame(dictionary,index=[0])
+#df.to_csv("billboard_data.csv")
+with open('billboard_data_7.csv', 'w') as f:  # Just use 'w' mode in 3.x
+    w = csv.DictWriter(f, dictionary.keys())
+    w.writeheader()
+    w.writerow(dictionary)
 
 
 
